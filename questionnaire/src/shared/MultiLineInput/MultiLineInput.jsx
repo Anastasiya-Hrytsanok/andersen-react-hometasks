f84040b935fd0f2af088ae62c195e9c47b6d0000
Nastya Gritsenok <nastyagritsenok@gmail.com>
2022-09-22 01:09:45 +0300

    fix: delete function

--- questionnaire/src/shared/MultiLineInput/MultiLineInput.jsx
+++ questionnaire/src/shared/MultiLineInput/MultiLineInput.jsx
@@ -1,18 +1,6 @@
 import React from 'react';
 
 class MultiLineInput extends React.Component {
-  handleKeyPress = (event) => {
-    let textarea = document.getElementById(this.props.label),
-      text = textarea.value,
-      numberOfLines = (text.match(/\n/g) || []).length + 1,
-      maxRows = parseInt(textarea.getAttribute('rows'));
-      console.log(textarea);
-
-    if (event.which === 7 && numberOfLines === maxRows) {
-      return false;
-    }
-  };
-
   render() {
     const { label, linesNumber, fieldName, placeholder } = this.props;
 
@@ -23,7 +11,6 @@ class MultiLineInput extends React.Component {
           id={label}
           placeholder={placeholder}
           rows={linesNumber}
-          onKeyPress={this.handleKeyPress}
           maxLength="590"
         ></textarea>
       </div>
