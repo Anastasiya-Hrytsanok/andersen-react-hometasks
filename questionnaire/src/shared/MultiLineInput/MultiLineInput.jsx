2989d8ea94bde5423c2476cdf5fe022114898e83
Nastya Gritsenok <nastyagritsenok@gmail.com>
2022-09-21 13:57:31 +0300

    fix: change fonts

--- questionnaire/src/shared/MultiLineInput/MultiLineInput.jsx
+++ questionnaire/src/shared/MultiLineInput/MultiLineInput.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 class MultiLineInput extends React.Component {
   handleKeyPress = (event) => {
-    var textarea = document.getElementById(this.props.label),
+    let textarea = document.getElementById(this.props.label),
       text = textarea.value,
       numberOfLines = (text.match(/\n/g) || []).length + 1,
       maxRows = parseInt(textarea.getAttribute('rows'));
       console.log(textarea);
 
-    if (event.which === 13 && numberOfLines === maxRows) {
+    if (event.which === 7 && numberOfLines === maxRows) {
       return false;
     }
   };
@@ -24,6 +24,7 @@ class MultiLineInput extends React.Component {
           placeholder={placeholder}
           rows={linesNumber}
           onKeyPress={this.handleKeyPress}
+          maxLength="590"
         ></textarea>
       </div>
     );
